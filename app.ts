9de86d2bfb96513191fc39205821fe6e0d63f185
Jason Poindexter <poindexter.json@gmail.com>
2024-07-07 12:29:19 -0700

    Cleanup

--- app.ts
+++ app.ts
@@ -16,14 +16,11 @@ function getUserDetails(email: string) {
     let startGetUserDetailsTime = new Date().getTime()
     let openSheetTime = new Date().getTime()
     const sheet = SpreadsheetApp.openById(SHEET_ID).getSheetByName(SHEET_NAME)
-    Logger.log(`Opening sheet took ${new Date().getTime() - openSheetTime} ms`)
     let startFetchSheetTime = new Date().getTime()
     const data = sheet.getDataRange().getValues()
-    Logger.log(`Fetching sheet took ${new Date().getTime() - startFetchSheetTime} ms`)
     const headers = data[0] // Fetch the headers
     let startFindTime = new Date().getTime()
     const resultString = data.find(row => row[EMAIL_ROW] === email) // Assuming details are in the 5th column\
-    Logger.log(`Finding email took ${new Date().getTime() - startFindTime} ms`)
     // Create a resultObj that contains the headers as keys and the values as values from RESULT_COLLUMNS
     const resultObj = {}
     if (resultString) {
@@ -34,21 +31,8 @@ function getUserDetails(email: string) {
             }
             resultObj[headers[i]] = resultString[i]
         })
-        Logger.log(
-            `getUserDetails took ${new Date().getTime() - startGetUserDetailsTime} ms`,
-        )
         return JSON.stringify(resultObj)
     } else {
-        Logger.log(
-            `getUserDetails took ${new Date().getTime() - startGetUserDetailsTime} ms`,
-        )
         return null
     }
-}
-
-function processForm(formObject: { searchString: string }) {
-    Logger.log('Processing form')
-    const template = HtmlService.createTemplateFromFile('index')
-    template.data = getUserDetails(formObject.searchString)
-    return template.evaluate().getContent()
 }
