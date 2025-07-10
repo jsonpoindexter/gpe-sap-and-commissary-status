16482807ef569b294a329637b685d83aed446414
Jason Poindexter <poindexter.json@gmail.com>
2025-07-10 12:58:05 -0700

    Implement doGet and doPost functions for handling HTTP requests and responses

--- app.ts
+++ app.ts
@@ -1,1 +1,33 @@
-export * from './http'
+function doGet() {
+    const htmlTemplate = HtmlService.createTemplateFromFile('index')
+    return htmlTemplate
+        .evaluate()
+        .setTitle('SAP, Eats, and Showers Status')
+        .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+}
+
+interface PostData {
+    secretKey: string
+    postProcessLastUpdate: string
+}
+
+function doPost(e: GoogleAppsScript.Events.DoPost): GoogleAppsScript.Content.TextOutput {
+    Logger.log('doPost called')
+    const properties: GoogleAppsScript.Properties.Properties =
+        PropertiesService.getScriptProperties()
+    if (e.postData.type === 'application/json') {
+        const postData: PostData = JSON.parse(e.postData.contents)
+        const { secretKey, postProcessLastUpdate } = postData
+        if (secretKey !== properties.getProperty('secretKey')) {
+            Logger.log('Unauthorized doPost request')
+            return ContentService.createTextOutput('Error: Unauthorized')
+        }
+        if (postProcessLastUpdate) {
+            Logger.log('Post Data: ', JSON.stringify(postData))
+            properties.setProperty('postProcessLastUpdate', postProcessLastUpdate)
+            return ContentService.createTextOutput('Update stored successfully.')
+        }
+    }
+
+    return ContentService.createTextOutput('Error: Invalid Content Type')
+}
